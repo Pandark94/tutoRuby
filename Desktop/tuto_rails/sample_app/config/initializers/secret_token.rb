18185155a45b2a6c8d9b4ebb78018dcdee0f8c0e
Emilien <emilien.lavie@telecom-paristech.fr>
2016-03-28 21:54:51 +0200

    fin chap3 statique

---
+++ Desktop/tuto_rails/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '62fb541f002a44b82cfd72b1c00ad651b49c643b7b92f72ae698fd296367599da4eab08d10ca3ec28d1bf7c3a35e90fa36bfef02314e27f82bd263c05fa08391'
