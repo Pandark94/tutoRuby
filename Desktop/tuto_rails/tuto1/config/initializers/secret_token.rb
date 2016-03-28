d340e4fb87dab0dda7ecc9dcf7b53fabe2aa1598
Emilien <emilien.lavie@telecom-paristech.fr>
2016-03-28 18:34:13 +0200

    Initial commit

---
+++ Desktop/tuto_rails/tuto1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuto1::Application.config.secret_token = 'c0f9a493f4f3c32835c29de6851fada786df5f88dd1893a3ba54812b6afa69127c74baaa9250e5eec628f19c759413c3a9da99ab1b583f558ca455fdcad9264d'
